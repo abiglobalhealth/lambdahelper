f6ed2062275d5821b9da7a1493af98198175e23a
Yonah Forst <yonaforst@hotmail.com>
2018-06-07 17:24:29 +0200

    added sns and invoke functions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var parse = require('./lib/parse')
+var invoke = require('./lib/invoke')
 var handler = require('./lib/handler')
 var lambdaProxyResponse = require('./lib/lambdaProxyResponse')
 var parseInvokeResponse = require('./lib/parseInvokeResponse')
+var sns = require('./lib/sns')
 
 module.exports = {
 	parse,
+	invoke,
 	handler,
 	lambdaProxyResponse,
 	parseInvokeResponse,
+	sns,
 }
